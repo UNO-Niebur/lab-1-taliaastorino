9f86b359b382a5b5908ad7b16041f59127834cf3
taliaastorino <tastorino@unomaha.edu>
2026-01-25 12:55:17 -0600

    Talia Astorino Update FirstProgram.py
    Updated and debugged Lab 1 Pt. 1.

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,12 +1,12 @@
 #FirstProgram.py
 #Talia Astorino:
 #January 25, 2026:
-#Lab 1:
+#Lab 1 Purpose: To ask a user for their name, and calculate their birth year.
 
 def main():
   print("First Program")
   
-  Hello!
+input("Hello!")
   #Ask for the user's name
 name = input("What is your name?")
   #Use the user's name in the program.
@@ -15,7 +15,7 @@ print(f"Hi, {name}, it is nice to meet you!")
 age = int(input("How old are you?"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-birth_year = 2026 - age
+birth_year = 2025 - age
 print(f"Your birth year is {birth_year}.")
 
 #Call the main function if this is the file being run.
