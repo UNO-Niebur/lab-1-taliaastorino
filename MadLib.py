2025ac88ba7e1b4d5ee2de41d74a957dcadd3ea4
taliaastorino <tastorino@unomaha.edu>
2026-01-25 13:30:46 -0600

    Talia Astorino Updated MadLib.py
    Updated Mad Lib for Lab 1.

--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,22 @@
 #MadLib.py
 #Talia Astorino:
 #January 25, 2026:
-#Lab 1:
+#Lab 1 Purpose: To print a story using user supplied words.
 
 def main():
   print("Madlib")
   #Ask user for words
-
+adjective1 = input("Enter an adjective")
+adjective2 = input("Enter another adjective")
+animal = input("Enter a noun")
+noun2 = input("Enter another noun")
+verb1 = input("Enter a verb")
+verb2 = input("Enter another verb")
   #Print the story with the user supplied words.
+story = ("One day, a " + adjective1 + " " + animal + " escaped from the zoo! It was very " + adjective2 + " and began " + verb1 + ". The " + noun2 + " came to " + verb2 + " the " + animal + " before returning it to the zoo.)
 
-
-
+print("Here is the story for you!")
+print(story)
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
