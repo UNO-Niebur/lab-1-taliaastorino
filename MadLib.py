f774fa6c4b7e072227b397e4891edc2ee922e974
taliaastorino <tastorino@unomaha.edu>
2026-01-25 13:46:06 -0600

    Talia Astorino Update MadLib.py
    Updated mad lib

--- MadLib.py
+++ MadLib.py
@@ -6,12 +6,12 @@
 def main():
   print("Madlib")
   #Ask user for words
-adjective1 = input("Enter an adjective")
-adjective2 = input("Enter another adjective")
-animal = input("Enter a noun")
-noun2 = input("Enter another noun")
-verb1 = input("Enter a verb")
-verb2 = input("Enter another verb")
+adjective1 = input("Enter an adjective. ")
+adjective2 = input("Enter another adjective. ")
+animal = input("Enter an animal. ")
+noun2 = input("Enter a noun. ")
+verb1 = input("Enter a verb. ")
+verb2 = input("Enter another verb. ")
   #Print the story with the user supplied words.
 story = ("One day, a " + adjective1 + " " + animal + " escaped from the zoo! It was very " + adjective2 + " and began " + verb1 + ". The " + noun2 + " came to " + verb2 + " the " + animal + " before returning it to the zoo.)
 
