ecdae2abd6b6338c0199d1ce0b3edfed70ade329
taliaastorino <tastorino@unomaha.edu>
2026-01-25 12:29:35 -0600

    Talia Astorino Update FirstProgram.py
    Lab 1 pt. 1 Complete

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Talia Astorino:
+#January 25, 2026:
+#Lab 1:
 
 def main():
   print("First Program")
-  #Say hello
   
+  Hello!
   #Ask for the user's name
-
+name = input("What is your name?")
   #Use the user's name in the program.
-
+print(f"Hi, {name}, it is nice to meet you!")
   #Ask the user for their age.
-
+age = int(input("How old are you?"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
+birth_year = 2026 - age
+print(f"Your birth year is {birth_year}.")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
